0f6b3442a780f47ae0d566fc965c8be14c2e84e6
steinoxyd <steinoxyd@gmail.com>
2014-01-11 14:23:23 +0100

    ParallelController Bugfix

--- src/ottocar_einparken/MoveToGap.cpp
+++ src/ottocar_einparken/MoveToGap.cpp
@@ -20,21 +20,21 @@ MoveToGap::driveData MoveToGap::moveToGap(ParallelController::orientationData da
 	driveData result;
 	result.speed.data = 0;
 
-	int angleValue = (TARGET_DISTANCE - data.distance) * 1000 * (-1);
-	if (angleValue < 50)
+	int angleValue = (TARGET_DISTANCE - data.distance) * 500 * (-1) * 1.2;
+	if (angleValue < 120)
 	{
-		result.angle.data = angleValue * 1.2;
+//		result.angle.data = angleValue;
 		result.speed.data = -8;
 
-//		result.angle.data = 0;
+		result.angle.data = -21;
 //		result.speed.data = 0;
 
 		ROS_INFO("[MTG]: Lenkung: %d | %2.4f", angleValue, data.distance);
 	}
 	else
 	{
-		result.angle.data = 0;
-		result.speed.data = 0;
+		result.angle.data = -21;
+		result.speed.data = -8;
 		ROS_WARN("[MTG]: Lenkung: %d | %2.4f", angleValue, data.distance);
 	}
 
